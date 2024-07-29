a74354bd0027eba3f69363cef167234ec89adc48
benjaminwfriedman <benny.friedman2@gmail.com>
2024-07-29 13:19:31 -0400

    updated test script to run on test DLR Buildings. Updated train test split to handle cluster vs unclustered better

--- data_utils/DLRGroupDataLoader.py
+++ data_utils/DLRGroupDataLoader.py
@@ -15,10 +15,21 @@ Creates:
 
 import os
 import numpy as np
-
+import pandas as pd
 from tqdm import tqdm
 from torch.utils.data import Dataset
 
+def build_complete_paths(data_root, data_type):
+    all_projects = []
+    for root, dirs, files in os.walk(data_root):
+        # Split the root path to check for the exact directory name
+        path_parts = root.split(os.sep)
+        if data_type in path_parts and path_parts[path_parts.index(data_type)] == data_type:
+            for file in files:
+                if file.endswith('.npy'):
+                    full_path = os.path.join(root, file)
+                    all_projects.append(full_path)
+    return all_projects
 
 class DLRGroupDataset(Dataset):
     def __init__(self, labels_path, test_project, split='train', data_root='trainval_fullarea',
@@ -33,15 +44,16 @@ class DLRGroupDataset(Dataset):
         with open(labels_path, 'r') as file:
             self.labels_length = len([line.strip() for line in file])
             
-        # Build Complete path for all .npy files
-        all_projects = []
-        for root, dirs, files in os.walk(data_root):
-            if data_type in root:
-                for file in files:
-                    if file.endswith('.npy'):
-                        full_path = os.path.join(root, file)
-                        all_projects.append(full_path)
+        # # Build Complete path for all .npy files
+        # all_projects = []
+        # for root, dirs, files in os.walk(data_root):
+        #     if data_type in root:
+        #         for file in files:
+        #             if file.endswith('.npy'):
+        #                 full_path = os.path.join(root, file)
+        #                 all_projects.append(full_path)
 
+        all_projects = build_complete_paths(data_root, data_type)
         # Split Data Between Train and test
         if split == 'train':
             projects_split = []
@@ -141,6 +153,104 @@ class DLRGroupDataset(Dataset):
 
     def __len__(self):
         return len(self.room_idxs)
+
+
+class DLRDatasetWholeScene():
+    # prepare to give prediction on each points
+    def __init__(self, root, block_points=4096, split='test', test_project="MorrisCollege_Pinson", stride=15.0, block_size=30.0, padding=0.001, data_type='clustered', labels_path="data_utils/labels_clean.txt"):
+        self.block_points = block_points
+        self.block_size = block_size
+        self.padding = padding
+        self.root = root
+        self.split = split
+        self.stride = stride
+        self.scene_points_num = []
+        assert split in ['train', 'test']
+        if self.split == 'train':
+            self.file_list = [d for d in os.listdir(f"{root}/{data_type}") if d.find(test_project) is not -1]
+        else:
+            self.file_list = [d for d in os.listdir(f"{root}/{data_type}/{test_project}") if d.find(test_project) is not -1]
+        self.scene_points_list = []
+        self.semantic_labels_list = []
+        self.room_labels_list = []
+        self.room_coord_min, self.room_coord_max = [], []
+        for file in self.file_list:
+            if ".npy" in file:
+                data = np.load(f"{root}/{data_type}/{test_project}/" + file)
+                points = data[:, :3]
+                self.scene_points_list.append(data[:, :6])
+                self.semantic_labels_list.append(data[:, 6])
+                coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
+                self.room_coord_min.append(coord_min), self.room_coord_max.append(coord_max)
+            if ".csv" in file:
+                df_rooms = pd.read_csv(f"{root}/{data_type}/{test_project}/" + file)
+                self.room_labels_list.append(df_rooms)
+        assert len(self.scene_points_list) == len(self.semantic_labels_list) == len(self.room_labels_list)
+
+        # Read Label File
+        with open(labels_path, 'r') as file:
+            self.labels_length = len([line.strip() for line in file])
+
+        labelweights = np.zeros(self.labels_length)
+        for seg in self.semantic_labels_list:
+            tmp, _ = np.histogram(seg, range(self.labels_length + 1))
+            self.scene_points_num.append(seg.shape[0])
+            labelweights += tmp
+        labelweights = labelweights.astype(np.float32)
+        labelweights = labelweights / np.sum(labelweights)
+        self.labelweights = np.power(np.amax(labelweights) / labelweights, 1 / 3.0)
+
+    def __getitem__(self, index):
+        point_set_ini = self.scene_points_list[index]
+        points = point_set_ini[:,:6]
+        labels = self.semantic_labels_list[index]
+        coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
+        grid_x = int(np.ceil(float(coord_max[0] - coord_min[0] - self.block_size) / self.stride) + 1)
+        grid_y = int(np.ceil(float(coord_max[1] - coord_min[1] - self.block_size) / self.stride) + 1)
+        data_room, label_room, sample_weight, index_room = np.array([]), np.array([]), np.array([]),  np.array([])
+        for index_y in range(0, grid_y):
+            for index_x in range(0, grid_x):
+                s_x = coord_min[0] + index_x * self.stride
+                e_x = min(s_x + self.block_size, coord_max[0])
+                s_x = e_x - self.block_size
+                s_y = coord_min[1] + index_y * self.stride
+                e_y = min(s_y + self.block_size, coord_max[1])
+                s_y = e_y - self.block_size
+                point_idxs = np.where(
+                    (points[:, 0] >= s_x - self.padding) & (points[:, 0] <= e_x + self.padding) & (points[:, 1] >= s_y - self.padding) & (
+                                points[:, 1] <= e_y + self.padding))[0]
+                if point_idxs.size == 0:
+                    continue
+                num_batch = int(np.ceil(point_idxs.size / self.block_points))
+                point_size = int(num_batch * self.block_points)
+                replace = False if (point_size - point_idxs.size <= point_idxs.size) else True
+                point_idxs_repeat = np.random.choice(point_idxs, point_size - point_idxs.size, replace=replace)
+                point_idxs = np.concatenate((point_idxs, point_idxs_repeat))
+                np.random.shuffle(point_idxs)
+                data_batch = points[point_idxs, :]
+                normlized_xyz = np.zeros((point_size, 3))
+                normlized_xyz[:, 0] = data_batch[:, 0] / coord_max[0]
+                normlized_xyz[:, 1] = data_batch[:, 1] / coord_max[1]
+                normlized_xyz[:, 2] = data_batch[:, 2] / coord_max[2]
+                data_batch[:, 0] = data_batch[:, 0] - (s_x + self.block_size / 2.0)
+                data_batch[:, 1] = data_batch[:, 1] - (s_y + self.block_size / 2.0)
+                data_batch[:, 3:6] /= 255.0
+                data_batch = np.concatenate((data_batch, normlized_xyz), axis=1)
+                label_batch = labels[point_idxs].astype(int)
+                batch_weight = self.labelweights[label_batch]
+
+                data_room = np.vstack([data_room, data_batch]) if data_room.size else data_batch
+                label_room = np.hstack([label_room, label_batch]) if label_room.size else label_batch
+                sample_weight = np.hstack([sample_weight, batch_weight]) if label_room.size else batch_weight
+                index_room = np.hstack([index_room, point_idxs]) if index_room.size else point_idxs
+        data_room = data_room.reshape((-1, self.block_points, data_room.shape[1]))
+        label_room = label_room.reshape((-1, self.block_points))
+        sample_weight = sample_weight.reshape((-1, self.block_points))
+        index_room = index_room.reshape((-1, self.block_points))
+        return data_room, label_room, sample_weight, index_room
+
+    def __len__(self):
+        return len(self.scene_points_list)
 
 
 if __name__ == '__main__':
--- train_semseg.py
+++ train_semseg.py
@@ -56,8 +56,9 @@ def parse_args():
     # Path to the annotation directory
     parser.add_argument('--data_dir', type=str, required=True, help='Directory where the data is stored')  # Added argument for data directory
     parser.add_argument('--test_project', type=str, required=True, help='Name of the Test Project')  # Added argument for test_poject name
-    parser.add_argument('--data_type', type=str, required=True, help='Type of Data Clustered or Unclustered') 
-    
+    parser.add_argument('--data_type', type=str, required=True, help='Type of Data Clustered or Unclustered')
+    parser.add_argument('--model_name', type=str, required=True, help='name of the model to be trained')
+
     return parser.parse_args()
 
 
@@ -231,7 +232,7 @@ def run(args):
 
         if epoch % 5 == 0:
             logger.info('Save model...')
-            savepath = str(checkpoints_dir) + '/model.pth'
+            savepath = str(checkpoints_dir) + f'/{args.model_name}.pth'
             log_string('Saving at %s' % savepath)
             state = {
                 'epoch': epoch,
